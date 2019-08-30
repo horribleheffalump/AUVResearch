dcc652ce3c08f17e8d970551681b2a45aa3a9131
Gregory Miller <gregbmiller@gmail.com>
2019-08-30 19:53:35 +0300

    Figures for model description

---
+++ DrawHelper.py
@@ -0,0 +1,96 @@
+import numpy as np
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
+def PX(A):
+    return np.array([0.0, A[1], A[2]])
+def PY(A):
+    return np.array([A[0], 0.0, A[2]])
+def PZ(A):
+    return np.array([A[0], A[1], 0.0])
+
+def X(A):
+    return np.array([A[0], 0.0, 0.0])
+def Y(A):
+    return np.array([0.0, A[1], 0.0])
+def Z(A):
+    return np.array([0.0, 0.0, A[2]])
+
+
+def drawPoint(ax, P, color = 'black', s=30, alpha = 1.0):
+    P = np.array(P)
+    return ax.scatter(P[0], P[1], P[2], color = color, s = s, alpha = alpha)
+
+def drawLine(ax, A, B, color = 'black', linestyle='-', linewidth=1.0, alpha = 1.0):
+    return ax.plot([A[0], B[0]], [A[1], B[1]], [A[2], B[2]], color = color, linestyle=linestyle, linewidth=linewidth, alpha=alpha)
+
+def drawArrow(ax, A,B, color = 'black', linewidth= 1.0):
+    deltaX = Arrow3D([A[0], B[0]], [A[1], B[1]], [A[2], B[2]], mutation_scale=10, arrowstyle="-|>", color=color, linewidth = linewidth)
+    return ax.add_artist(deltaX)
+
+def textAtPoint(ax, A, string, shift = [0.0,0.0,0.0]):
+    return ax.text(A[0]+shift[0], A[1]+shift[1], A[2]+shift[2], string)
+
+def cart2sphere(X: np.array):
+    r = np.sqrt(X[0] * X[0] + X[1] * X[1] + X[2] * X[2])
+    theta = np.arctan2(X[1], X[0]);
+    gamma = np.arctan2(X[2], X[0] / np.cos(theta));
+    return r, gamma, theta
+
+
+def drawArcAngles(ax, C, P, theta: float, gamma: float, N=100):
+    #ax - axes, C - center, P - point on the circle where to start the arc, theta, gamma - spheric angles (define length and direction of the arc), N - number of points on the arc
+    tol = 1e-10     
+    C = np.array(C)
+    P = np.array(P)
+    if np.abs(theta) > 0:
+        th = np.arange(0,theta+tol*np.sign(theta), theta / N)
+    else:
+        th = np.zeros(N)
+        
+    if np.abs(gamma) > 0:
+        gm = np.arange(0,gamma+tol*np.sign(gamma), gamma / N)
+    else:
+        gm = np.zeros(N)
+
+        
+    r, gm0, th0 = cart2sphere(P-C) 
+
+    x = np.array(list(map(lambda i: C[0] + r * np.cos(gm[i]+gm0) * np.cos(th[i]+th0), np.arange(0, N, 1))))
+    y = np.array(list(map(lambda i: C[1] + r * np.cos(gm[i]+gm0) * np.sin(th[i]+th0), np.arange(0, N, 1))))
+    z = np.array(list(map(lambda i: C[2] + r * np.sin(gm[i]+gm0), np.arange(0, N, 1))))
+    return ax.plot(x, y, z, color = 'black', linewidth = 0.5)
+
+
+def drawArc(ax, C, P, D, N=20):
+    #ax - axes, C - center, P - point on the circle where to start the arc, D - third point defines the direction where the arc ends, N - number of points on the arc
+    C = np.array(C)
+    P = np.array(P)
+    D = np.array(D)
+    _, gm0, th0 = cart2sphere(P-C) 
+    _, gm1, th1 = cart2sphere(D-C) 
+    return drawArcAngles(ax, C, P, th1-th0, gm1-gm0, N)
+
+
+def drawArcScale(ax, C, P, D, scale=1.0, N=100):
+    #ax - axes, C - center, P - point on the circle where to start the arc, D - third point defines the direction where the arc ends, N - number of points on the arc
+    C = np.array(C)
+    P = np.array(P)
+    D = np.array(D)
+    _, gm0, th0 = cart2sphere(P-C) 
+    _, gm1, th1 = cart2sphere(D-C) 
+
+    return drawArcAngles(ax, C, C + (P - C) * scale, th1-th0, gm1-gm0, N)
+
+
---
+++ figures.py
@@ -0,0 +1,42 @@
+
+from Seabed import *
+from AUVControlled import *
+from CMNFFilter import *
+from testenvironmentControlled import *
+from math import *
+#np.random.seed(2213123)
+
+seabed = Seabed()
+estimateslope = True
+
+pc=28.0
+pr=20.0
+tr=20.0
+
+accuracy = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+PhiBounds =     [[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr],[pc-pr,pc+pr]]
+ThetaBounds =   [[10.0+tr,10.0-tr],   [100.0+tr,100.0-tr],  [190.0+tr,190.0-tr],  [280.0+tr,280.0-tr], [55.0+tr,55.0-tr],   [145.0+tr,145.0-tr],  [235.0+tr,235.0-tr],  [325.0+tr,325.0-tr]]
+NBeams = 10
+
+TT = 360.0
+T = 360.0
+delta = 1.0
+N = int(T / delta)
+
+X0 = np.array([5,5,-10])
+DW = np.array([0.5,0.5,0.5])
+U = lambda t: np.pi * np.array([1 / 100.0 * np.cos(1.0 * np.pi * t / TT), 1 / 3.0 * np.cos(4.0 * np.pi * t / TT)])
+#U = lambda t: np.array([np.pi/2 - np.pi  / 100.0 * np.cos(1.0 * np.pi * t / TT), np.pi / 3.0 * np.cos(4.0 * np.pi * t / TT)])
+v = 2.0 # 5.0
+t_history = np.arange(0.0, T + delta, delta)
+VNominal_history = np.array(list(map(lambda t: AUVControlled.V(v, U(t)), t_history[:-1])))
+deltaXNominal_history = np.vstack((np.zeros(X0.shape), delta * VNominal_history))
+XNominal_history = X0 + np.cumsum(deltaXNominal_history, axis = 0)
+
+auv = AUVControlled(T, delta, X0, DW, U, v)
+test = testenvironmentControlled(T, delta, NBeams, accuracy, PhiBounds, ThetaBounds, auv, seabed, estimateslope)
+
+
+test.showsonarmodel('D:\\Наука\\_Статьи\\__в работе\\navigation\\fig_3_sensor_orig.pdf', False)
+test.showoptimalcontrol('D:\\Наука\\_Статьи\\__в работе\\navigation\\fig_2_optcontrol_orig.pdf', False)
+test.showmodel('D:\\Наука\\_Статьи\\__в работе\\navigation\\fig_1_model_orig.pdf', False)
